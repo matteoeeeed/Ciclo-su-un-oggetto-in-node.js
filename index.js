886806f1a774911bbb4151c8fac4930d03cf0a90
matteoeeeed <95379963+matteoeeeed@users.noreply.github.com>
2023-02-05 10:56:38 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//importate nell'index persona
+const nomee=require('./nome')
+
+console.log(nomee)
+console.log(" utilizzo keys nell'oggetto:  "+Object.keys(nomee))
+console.log(nomee)
+
+
+//utilizzare persona ...(attenti)
+// voglio salutare un solo nome dei nomi importati (dal modulo persona)
